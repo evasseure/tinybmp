c2fda0877dfaa53c45447b57b9b567e0bea2a4e7
Erwan <e.vasseure@gmail.com>
2022-03-22 21:42:31 +0100

    feat: added SetColor

--- draw.go
+++ draw.go
@@ -1,19 +1,30 @@
 package bmp
 
-// DrawPoint sets the pixel with (r, g, b)
-func DrawPoint(pixel *Pixel, r, g, b int) {
-	pixel.R = clampRGB(r)
-	pixel.G = clampRGB(g)
-	pixel.B = clampRGB(b)
+var colorR = 0
+var colorG = 0
+var colorB = 0
+
+// SetColor sets the color used for drawing
+func SetColor(r, g, b int) {
+	colorR = clampRGB(r)
+	colorG = clampRGB(g)
+	colorB = clampRGB(b)
 }
 
-// DrawRect IT IS MISSING THE COLOR
+// DrawPoint sets the pixel with (r, g, b)
+func DrawPoint(pixels [][]Pixel, x, y int) {
+	pixels[x][y].R = colorR
+	pixels[x][y].G = colorG
+	pixels[x][y].B = colorB
+}
+
+// DrawRect draw a filled rectangle at (x, y) with (w, h) width and height
 func DrawRect(pixels [][]Pixel, x, y, w, h int) [][]Pixel {
 	for j := 0; j < h; j++ {
 		for i := 0; i < w; i++ {
-			pixels[x+i][y+j].B = 255
-			pixels[x+i][y+j].G = 0
-			pixels[x+i][y+j].R = 0
+			pixels[x+i][y+j].R = colorR
+			pixels[x+i][y+j].G = colorG
+			pixels[x+i][y+j].B = colorB
 		}
 	}
 	return pixels
